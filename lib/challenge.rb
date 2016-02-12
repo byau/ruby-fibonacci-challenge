c12f062b5243dac38f0850c5d5449213007b07e0
byau <billy.l.yau@gmail.com>
2016-02-12 14:35:27 -0500

    add fibrange

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,4 +1,23 @@
 class FibonacciRange
+  include Enumerable
+
+  def initialize (stop)
+    @stop = stop
+  end
+
+  def each
+    current_fib = 0
+    next_fib = 1
+    while current_fib <= @stop
+      yield current_fib
+      previous_fib = current_fib
+      current_fib = next_fib
+      next_fib = current_fib + previous_fib
+    end
+  end
+
+
+
 end
 
 class FibonacciNumber
